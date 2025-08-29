570d909b27220dc2a7e3703bde142a132a0bbbe7
Marwan Mohamed Zein <marawanzein222@gmail.com>
2025-08-30 01:29:17 +0300

    Delete test.sql

--- test.sql
+++
@@ -1,8 +0,0 @@
--- ALTER TABLE cars DROP COLUMN car_id
--- select * from cars
-select * from cars;
--- delete from cars;
--- insert into plans("value", "unit", "price") VALUES (1, 'day', 50);
--- SELECT column_name, data_type
--- FROM information_schema.columns
--- WHERE table_name = 'plans';
